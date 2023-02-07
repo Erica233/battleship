2b0b98993656a1ae3165479e0f391bf5081ac049
Fangting Ma <fm128@duke.edu>
2023-02-07 16:20:14 -0500

    modified

--- app/src/main/java/edu/duke/fm128/battleship/BattleShipBoard.java
+++ app/src/main/java/edu/duke/fm128/battleship/BattleShipBoard.java
@@ -1,9 +1,16 @@
 package edu.duke.fm128.battleship;
 
+
 public class BattleShipBoard implements Board {
   private final int width;
   private final int height;
 
+  /**
+   * Constructs a BattleShipBoard with the specified width and height
+   * @param w is the width of the newly constructed board.
+   * @param h is the height of the newly constructed board.
+   * @throws IllegalArgumentException if the width or height are less than or equal to zero.
+   */
   public BattleShipBoard(int w, int h) {
     if (w <= 0) {
       throw new IllegalArgumentException("BattleShipBoard's width must be positive but is " + w);
