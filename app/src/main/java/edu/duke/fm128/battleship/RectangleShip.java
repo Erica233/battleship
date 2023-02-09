af10f1a8adf37bbee36a2b2d1aa51e5d370d9309
Fangting Ma <fm128@duke.edu>
2023-02-08 23:10:38 -0500

    1

--- app/src/main/java/edu/duke/fm128/battleship/RectangleShip.java
+++ app/src/main/java/edu/duke/fm128/battleship/RectangleShip.java
@@ -3,28 +3,30 @@ package edu.duke.fm128.battleship;
 import java.util.HashSet;
 
 public class RectangleShip {
-    /**
-     * Generates the set of coordinates for a rectangle starting at upperLeft
-     * whose width and height are as specified
-     *
-     * @param upperLeft the coordinate of the upper left part of the ship
-     * @param width the width of the ship
-     * @param height the height of the ship
-     * @return the set of coordinates occupied by the ship
-     */
-    public static HashSet<Coordinate> makeCoords(Coordinate upperLeft, int width, int height) {
-        HashSet<Coordinate> coordsSet = new HashSet<>();
-        for (int r = 0; r < height; r++) {
-          for (int c = 0; c < width; c++) {
-              coordsSet.add(new Coordinate(upperLeft.getRow() + r, upperLeft.getColumn() + c));
-          }
-        }
-        return coordsSet;
+  /**
+   * Generates the set of coordinates for a rectangle starting at upperLeft
+   * whose width and height are as specified
+   *
+   * @param upperLeft the coordinate of the upper left part of the ship
+   * @param width     the width of the ship
+   * @param height    the height of the ship
+   * @return the set of coordinates occupied by the ship
+   */
+  public static HashSet<Coordinate> makeCoords(Coordinate upperLeft, int width, int height) {
+    HashSet<Coordinate> coordsSet = new HashSet<>();
+    for (int r = 0; r < height; r++) {
+      for (int c = 0; c < width; c++) {
+        coordsSet.add(new Coordinate(upperLeft.getRow() + r, upperLeft.getColumn() + c));
+      }
     }
+    return coordsSet;
+  }
 
-    /*public RectangleShip(Coordinate upperLeft, int width, int height) {
-        //super(makeCoords(upperLeft, width, height)); //specify how to call parent class constructor
-    }
-    */
+  /*
+   * public RectangleShip(Coordinate upperLeft, int width, int height) {
+   * //super(makeCoords(upperLeft, width, height)); //specify how to call parent
+   * class constructor
+   * }
+   */
 
 }
--- app/src/test/java/edu/duke/fm128/battleship/RectangleShipTest.java
+++ app/src/test/java/edu/duke/fm128/battleship/RectangleShipTest.java
@@ -8,13 +8,13 @@ import java.util.HashSet;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleShipTest {
-    @Test
-    void test_make_coords() {
-        HashSet<Coordinate> realSet = RectangleShip.makeCoords(new Coordinate(1, 2), 1, 3);
-        HashSet<Coordinate> expectedSet = new HashSet<>();
-        expectedSet.add(new Coordinate(1, 2));
-        expectedSet.add(new Coordinate(2, 2));
-        expectedSet.add(new Coordinate(3, 2));
-        assertEquals(expectedSet, realSet);
-    }
+  @Test
+  void test_make_coords() {
+    HashSet<Coordinate> realSet = RectangleShip.makeCoords(new Coordinate(1, 2), 1, 3);
+    HashSet<Coordinate> expectedSet = new HashSet<>();
+    expectedSet.add(new Coordinate(1, 2));
+    expectedSet.add(new Coordinate(2, 2));
+    expectedSet.add(new Coordinate(3, 2));
+    assertEquals(expectedSet, realSet);
+  }
 }
