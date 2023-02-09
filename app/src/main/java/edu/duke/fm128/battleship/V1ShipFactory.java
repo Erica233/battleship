9587fa37032b58196d47f14d10da88ff945becdb
Fangting Ma <fangtingm@student.unimelb.edu.au>
2023-02-09 06:58:36 -0500

    goal2 task12 V1ShipFactory.java

--- app/src/main/java/edu/duke/fm128/battleship/V1ShipFactory.java
+++ app/src/main/java/edu/duke/fm128/battleship/V1ShipFactory.java
@@ -3,8 +3,16 @@ package edu.duke.fm128.battleship;
 public class V1ShipFactory implements AbstractShipFactory<Character> {
 
   protected Ship<Character> createShip(Placement where, int w, int h, char letter, String name) {
-
-    return null;
+    int width = w;
+    int height = h;
+    if (where.getOrientation() != 'V' && where.getOrientation() != 'H') {
+      throw new IllegalArgumentException("The orientation of the placement should be horizontal or vertical!");
+    }
+    if (where.getOrientation() == 'H') {
+      width = h;
+      height = w;
+    }
+    return new RectangleShip<>(name, where.getWhere(), width, height, letter, '*');
   }
 
   @Override
@@ -13,17 +21,17 @@ public class V1ShipFactory implements AbstractShipFactory<Character> {
   }
 
   @Override
-  public Ship<Character> makeBattleship(Placement where) {
+  public Ship<Character> makeDestroyer(Placement where) {
     return createShip(where, 1, 3, 'd', "Destroyer");
   }
 
   @Override
-  public Ship<Character> makeCarrier(Placement where) {
+  public Ship<Character> makeBattleship(Placement where) {
     return createShip(where, 1, 4, 'b', "Battleship");
   }
 
   @Override
-  public Ship<Character> makeDestroyer(Placement where) {
+  public Ship<Character> makeCarrier(Placement where) {
     return createShip(where, 1, 6, 'c', "Carrier");
   }
 
