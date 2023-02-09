01162f3087dbbc0353a3c4092d0cddb7e2b5954e
Fangting Ma <fm128@duke.edu>
2023-02-09 15:44:39 -0500

    goal2 task14

--- app/src/main/java/edu/duke/fm128/battleship/NoCollisionRuleChecker.java
+++ app/src/main/java/edu/duke/fm128/battleship/NoCollisionRuleChecker.java
@@ -8,30 +8,31 @@ import java.nio.charset.CoderResult;
  * @param <T>
  */
 public class NoCollisionRuleChecker<T> extends PlacementRuleChecker<T> {
-    /**
-     * Constructs a NoCollisionRuleChecker
-     *
-     * @param next the next NoCollisionRuleChecker
-     */
-    public NoCollisionRuleChecker(PlacementRuleChecker<T> next) {
-        super(next);
-    }
+  /**
+   * Constructs a NoCollisionRuleChecker
+   *
+   * @param next the next NoCollisionRuleChecker
+   */
+  public NoCollisionRuleChecker(PlacementRuleChecker<T> next) {
+    super(next);
+  }
 
-    /**
-     * checks the rule that theShip does not collide with anything else on the Board
-     * (that all the squares it needs are empty)
-     *
-     * @param theShip  the ship to check
-     * @param theBoard the board to check
-     * @return true if theShip does not collide with anything else on the Board, otherwise false
-     */
-    @Override
-    protected boolean checkMyRule(Ship<T> theShip, Board<T> theBoard) {
-        for (Coordinate c: theShip.getCoordinates()) {
-            if (theBoard.whatIsAt(c) != null) {
-                return false;
-            }
-        }
-        return true;
+  /**
+   * checks the rule that theShip does not collide with anything else on the Board
+   * (that all the squares it needs are empty)
+   *
+   * @param theShip  the ship to check
+   * @param theBoard the board to check
+   * @return true if theShip does not collide with anything else on the Board,
+   *         otherwise false
+   */
+  @Override
+  protected boolean checkMyRule(Ship<T> theShip, Board<T> theBoard) {
+    for (Coordinate c : theShip.getCoordinates()) {
+      if (theBoard.whatIsAt(c) != null) {
+        return false;
+      }
     }
+    return true;
+  }
 }
