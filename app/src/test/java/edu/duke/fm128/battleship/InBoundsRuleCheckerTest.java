b64aceb0132da762852c00ed44698f4606794ba0
Fangting Ma <fm128@duke.edu>
2023-02-09 14:53:11 -0500

    goal2 task13

--- app/src/test/java/edu/duke/fm128/battleship/InBoundsRuleCheckerTest.java
+++ app/src/test/java/edu/duke/fm128/battleship/InBoundsRuleCheckerTest.java
@@ -6,26 +6,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InBoundsRuleCheckerTest {
 
-    @Test
-    void test_check_my_rule() {
-        BattleShipBoard<Character> b1 = new BattleShipBoard<>(10, 20);
-        V1ShipFactory f = new V1ShipFactory();
-        InBoundsRuleChecker<Character> checker = new InBoundsRuleChecker<>(null);
-        Placement p0 = new Placement(new Coordinate(1, 2), 'V');
-        Placement p1 = new Placement(new Coordinate(-1, 2), 'V');
-        Placement p2 = new Placement(new Coordinate(1, -2), 'H');
-        Placement p3 = new Placement(new Coordinate(100, 2), 'V');
-        Placement p4 = new Placement(new Coordinate(1, 200), 'V');
-        Ship<Character> sub0 = f.makeSubmarine(p0);
-        Ship<Character> sub1 = f.makeSubmarine(p1);
-        Ship<Character> sub2 = f.makeSubmarine(p2);
-        Ship<Character> sub3 = f.makeSubmarine(p3);
-        Ship<Character> sub4 = f.makeSubmarine(p4);
-        assertTrue(checker.checkMyRule(sub0, b1));
-        assertFalse(checker.checkMyRule(sub1, b1));
-        assertFalse(checker.checkMyRule(sub2, b1));
-        assertFalse(checker.checkMyRule(sub3, b1));
-        assertFalse(checker.checkMyRule(sub4, b1));
-
-    }
+  @Test
+  void test_check_my_rul_and_placemente() {
+    BattleShipBoard<Character> b1 = new BattleShipBoard<>(10, 20);
+    V1ShipFactory f = new V1ShipFactory();
+    InBoundsRuleChecker<Character> checker = new InBoundsRuleChecker<>(null);
+    Placement p0 = new Placement(new Coordinate(1, 2), 'V');
+    Placement p1 = new Placement(new Coordinate(-1, 2), 'V');
+    Placement p2 = new Placement(new Coordinate(1, -2), 'H');
+    Placement p3 = new Placement(new Coordinate(100, 2), 'V');
+    Placement p4 = new Placement(new Coordinate(1, 200), 'V');
+    Ship<Character> sub0 = f.makeSubmarine(p0);
+    Ship<Character> sub1 = f.makeSubmarine(p1);
+    Ship<Character> sub2 = f.makeSubmarine(p2);
+    Ship<Character> sub3 = f.makeSubmarine(p3);
+    Ship<Character> sub4 = f.makeSubmarine(p4);
+    assertTrue(checker.checkPlacement(sub0, b1));
+    assertFalse(checker.checkPlacement(sub1, b1));
+    assertFalse(checker.checkPlacement(sub2, b1));
+    assertFalse(checker.checkPlacement(sub3, b1));
+    assertFalse(checker.checkPlacement(sub4, b1));
+    assertTrue(checker.checkMyRule(sub0, b1));
+    assertFalse(checker.checkMyRule(sub1, b1));
+    assertFalse(checker.checkMyRule(sub2, b1));
+    assertFalse(checker.checkMyRule(sub3, b1));
+    assertFalse(checker.checkMyRule(sub4, b1));
+  }
 }
